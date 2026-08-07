837726a50d5167a8fca005db342677fd8bf48e48
karawaqqe <146225027+karawaqqe@users.noreply.github.com>
2026-08-07 15:21:58 +0200

    Fix contact form patterns

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function ContactForm({ onAddContact }) {
           name="name"
           value={name}
           onChange={handleChange}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          pattern="\p{L}+(([' \-]\p{L}+)?\p{L}*)*"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
@@ -51,7 +51,7 @@ function ContactForm({ onAddContact }) {
           name="number"
           value={number}
           onChange={handleChange}
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
         />
